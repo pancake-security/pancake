55965893b35ba72e172488a3d9a157e014a5369d
Lloyd Brown <33502227+lab352@users.noreply.github.com>
2019-10-06 12:41:04 -0700

    Update libclient/src/client.h
    Co-Authored-By: Anurag Khandelwal <anuragk@berkeley.edu>

--- libclient/src/client.h
+++ libclient/src/client.h
@@ -14,7 +14,7 @@ public:
     virtual std::string get(const std::string &key) = 0;
     virtual void put(const std::string &key, const std::string &value) = 0;
     virtual std::vector<std::string> get_batch(const std::vector<std::string> &keys) = 0;
-    virtual void put_batch(const std::vector<std::string> keys, const std::vector<std::string> values) = 0;
+    virtual void put_batch(const std::vector<std::string> &keys, const std::vector<std::string> &values) = 0;
 
 };
 #endif //CLIENT_H
